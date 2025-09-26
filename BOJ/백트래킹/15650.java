bb67317fbc187ffb121b35371d7393ab9193c388
Minyeong Park <1030pmy@gmail.com>
2025-09-26 22:25:44 +0900

    Update 15650.java

--- BOJ/백트래킹/15650.java
+++ BOJ/백트래킹/15650.java
@@ -1,16 +1,15 @@
+#1
 import java.util.*;
 import java.io.*;
 
 class Main {
     static int n, m;
     static int[] arr;
-    static boolean[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
         arr = new int[n + 1];
-        visited = new boolean[n + 1];
         function(1, 0);
     }
     private static void function(int idx, int depth) {
@@ -22,12 +21,41 @@ class Main {
             return;
         }
         for (int i = idx; i <= n; i++) {
-            if (!visited[i]) {
                 arr[depth] = i;
-                visited[i] = true;
                 function(i + 1, depth + 1);
-                visited[i] = false;
-            }
         }
+    }
+}
+
+#2
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static int[] arr = new int[10];
+
+    public static void func(int k) {
+        if (k == m) { // m개를 모두 택했으면
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        int st = 1; 
+        if (k != 0) st = arr[k - 1] + 1; // 이전 값보다 1 큰 수부터 시작
+
+        for (int i = st; i <= n; i++) {
+            arr[k] = i;     // k번째 수를 i로 정함
+            func(k + 1);    // 다음 자리 선택
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        func(0);
     }
 }

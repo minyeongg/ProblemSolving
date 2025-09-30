1dce9d141ac33fad19d6489cfd65ab6e3b49533f
Minyeong Park <1030pmy@gmail.com>
2025-09-30 20:26:21 +0900

    Create 15656.java

---
+++ BOJ/백트래킹/15656.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    static int n, m;
+    static int[] num;    // 입력 받은 숫자 저장
+    static int[] arr;    // 선택한 숫자 저장
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        num = new int[n];
+        arr = new int[m];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num); // 정렬
+        func(0);
+
+        System.out.print(sb);
+    }
+    
+    private static void func(int cnt) throws IOException {
+        if (cnt == m) {
+            for (int i : arr) {
+                sb.append(num[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+                arr[cnt] = i;
+                func(cnt + 1);
+        }
+    }
+}

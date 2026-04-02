6ed2ff47a9e95816b91d4d90712aa6f9ca59d8aa
Minyeong Park <1030pmy@gmail.com>
2026-04-02 10:04:55 +0900

    Title: 최소 스패닝 트리, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260402/최소 스패닝 트리/minimum-spanning-tree.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] edges = new int[m][3];
+        for (int i = 0; i < m; i++) {
+            edges[i][0] = sc.nextInt();
+            edges[i][1] = sc.nextInt();
+            edges[i][2] = sc.nextInt();
+        }
+        // Please write your code here.
+    }
+}

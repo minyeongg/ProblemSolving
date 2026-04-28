650888ee08a46e3728ab873832149f90f1d4d6a7
Minyeong Park <1030pmy@gmail.com>
2026-04-28 12:49:42 +0900

    Title: 피보나치 수, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260428/피보나치 수/fibonacci-number.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] memo = new int[50];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        Arrays.fill(memo, -1);
+
+        System.out.println(fibo(n));
+    }
+
+    static int fibo(int n) {
+        if (memo[n] == -1) {
+            memo[n] = fibo(n - 1) + fibo(n - 2);
+        }
+        return memo[n];
+    }
+}

59888348485bd91489ae6bbc097d70bfffd7d962
Minyeong Park <1030pmy@gmail.com>
2026-06-02 10:25:35 +0900

    [Passed] a로 채워넣기 (124ms, 10MB)

---
+++ 260602/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int len = str.length();
+        str = str.substring(0, 1) + "a" + str.substring(2);
+        str = str.substring(0, len - 2) + "a" + str.substring(len - 1);
+        System.out.println(str);
+    }
+}

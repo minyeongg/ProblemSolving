259506b3dc60be27e8aa671b4e2daf71717a30ad
Minyeong Park <1030pmy@gmail.com>
2026-07-24 09:46:28 +0900

    Update 양과 늑대.java

--- Programmers/트리/양과 늑대.java
+++ Programmers/트리/양과 늑대.java
@@ -16,8 +16,43 @@ class 양과 늑대 {
 
   private static ArrayList<Integer>[] tree;
 
-  private static void buildTree(
+  private static void buildTree(int[] info, int[][] edges) {
+    tree = new ArrayList[info.length];
+    for (int i = 0; i < tree.length; i++) {
+      tree[i] = new ArrayList<>();
+    }
+
+    for (int[] edge : edges) {
+      tree[edge[0]].add(edge[1]);
+    }
+  }
   public int solution(int[] info, int[][] edges) {
-    
+    buildTree(info, edges);
+    int answer = 0;
+
+    ArrayDeque<Info> q = new ArrayDeque<>();
+    q.add(new Info(0, 1, 0, new HashSet<>()));
+
+    while (!q.isEmpty()) {
+      Info now = q.poll();
+      answer = Math.max(answer, now.sheep);
+      now.visited.addAll(tree[now.node]);
+
+      for (int next : now.visited) {
+         HashSet<Integer> set = new HashSet<>(now.visited);
+         set.remove(next);
+
+        if (info[next] == 1) {
+          if (now.sheep != now.wolf + 1) {
+            q.add(new Info(next, now.sheep, now.wolf + 1, set));
+          }
+        }
+        else {
+          q.add(new Info(next, now.sheep + 1, now.wolf, set));
+        }
+      }
+    }
+
+    return answer;
   }
 }

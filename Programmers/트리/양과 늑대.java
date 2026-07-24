4cfe6c58cd0a16945e6a0f13e876267e1d1d72c7
Minyeong Park <1030pmy@gmail.com>
2026-07-24 09:03:56 +0900

    Create 양과 늑대.java

---
+++ Programmers/트리/양과 늑대.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class 양과 늑대 {
+  private static class Info {
+    int node, sheep, wolf;
+    HashSet<Integer> visited;
+
+    public Info(int node, int sheep, int wolf, HashSet<Integer> visited) {
+      this.node = node;
+      this.sheep = sheep;
+      this.wolf = wolf;
+      this.visited = visited;
+    }
+  }
+
+  private static ArrayList<Integer>[] tree;
+
+  private static void buildTree(
+  public int solution(int[] info, int[][] edges) {
+    
+  }
+}
